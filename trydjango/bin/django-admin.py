595853806b7c3802d53a516728462f08bd22e35d
angejr <ericmrleliko@gmail.com>
2022-12-08 20:23:50 +0100

    Project start

---
+++ trydjango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/thoody-3/Documents/Personal_projects/Django_tutorial/trydjango/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
